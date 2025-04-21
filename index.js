3ecf534411595d3adbb613cf4788b53198560afd
Viccenzo Gottardo Boff <viccenzoboff@gmail.com>
2025-04-21 16:35:43 -0300

    aqui eu criei os caminhos da API

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import banco from "./banco.js";
+import editora from "./controller/EditoraController.js";
+import autor from "./controller/AutorController.js";
+import emprestimo from "./controller/EmprestimoController.js";
+
+try {
+    await banco.authenticate();
+    console.log('Conexão com o banco de dados realizada com sucesso.');
+} catch (error) {
+    console.error('Erro ao conectar com o banco de dados:', error);
+}
+
+const app = express();
+app.use(express.json());
+
+app.get('/teste', (req, res) => {
+    res.send('Teste ok.');
+});
+
+//rotas crud da tabela editora
+app.get('/editora', editora.listar);
+app.get('/editora/:id', editora.selecionar);
+app.post('/editora', editora.inserir);
+app.put('/editora/:id', editora.alterar);
+app.delete('/editora/:id', editora.excluir);
+
+app.get('/autor', autor.listar);
+app.get('/autor/:id', autor.selecionar);
+app.post('/autor', autor.inserir);
+app.put('/autor/:id', autor.alterar);
+app.delete('/autor/:id', autor.excluir);
+
+app.get('/emprestimo', emprestimo.listar);
+app.get('/emprestimo/:id', emprestimo.selecionar);
+app.post('/emprestar', emprestimo.emprestar);
+app.put('/devolver/:id', emprestimo.devolver);
+
+app.listen(3000, () => { console.log(`Servidor rodando.`) });
